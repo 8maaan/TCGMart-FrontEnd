ace382015994e573a348852f6f4d9db91b5e1e58
Ribo <116358794+8maaan@users.noreply.github.com>
2023-11-29 12:54:40 +0800

    Fixed error

--- src/Navigations/firstNavi.js
+++ src/Navigations/firstNavi.js
@@ -1,35 +1,36 @@
 import './firstNavi.css';
 import { Link } from 'react-router-dom';
 
-
-export default function FirstNavi(){
+export default function FirstNavi() {
     const isLoggedIn = localStorage.getItem("isLoggedIn");
     const username = localStorage.getItem("username");
+
     return (
         <>
-        <div className="first-navi-main">
-            <div className='navi-left'>
-                <Link to='/'><h1>TCGMart</h1></Link>
-                <Link to='/'><h2>Home</h2></Link>
-                <Link to='/marketpage'><h2>Market</h2></Link>
-                <h2>Guide</h2>
+            <div className="first-navi-main">
+                <div className='navi-left'>
+                    <Link to='/'><h1>TCGMart</h1></Link>
+                    <Link to='/'><h3>Home</h3></Link>
+                    <Link to='/marketpage'><h3>Market</h3></Link>
+                    <h3>Guide</h3>
+                </div>
+                <div className='navi-right'>
+                    {isLoggedIn === 'true' ? (
+                        <>
+                            <Link to='/listings'><h3>Listings</h3></Link>
+                            <Link to='/sellcard'><h3>Sell Card</h3></Link>
+                            <Link to='/profile' className='navi-username-area'>
+                                <div className='navi-profile-picture'>
+                                    <img src='https://i.imgur.com/BFPW8Kw.png' alt="user img" className='user-profile-picture' />
+                                </div>
+                                <h3>{username}</h3>
+                            </Link>
+                        </>
+                    ) : (
+                            <Link to='/login'><h3>Login/Register</h3></Link>
+                        )}
+                </div>
             </div>
-            <div className='navi-right'>
-                {isLoggedIn === 'true' ? (
-                    <>
-                        <Link to='/listings'><h2>Listings</h2></Link>
-                        <Link to='/sellcard'><h2>Sell Card</h2></Link>
-                        <Link to='/profile' className='navi-username-area'>
-                            <div className='navi-profile-picture'>
-                                <img src='https://i.imgur.com/BFPW8Kw.png' alt="User profile picture" className='user-profile-picture'/>
-                            </div>
-                            <h2>{username}</h2>
-                    </>
-                ) : (
-                    <Link to='/login'><h2>Login/Register</h2></Link>
-                )}
-            </div>
-        </div>
         </>
     );
 }
